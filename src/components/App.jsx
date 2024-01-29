d2ffc16ed4c05c9d32f01502c9cb42550ece210d
Michal Viwegier <beebas@wp.pl>
2024-01-29 06:10:34 +0100

    Created basic file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import css from './App.module.css';
+
+import { FallingLines } from 'react-loader-spinner';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          // height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // fontSize: '2em',
+          // color: '#010101',
+          border: '1px solid red',
+        }}
+      >
+        React homework template
+      </div>
+      <div
+        style={{
+          // height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // fontSize: '2em',
+          // color: '#010101',
+          border: '1px solid green',
+        }}
+      >
+        <h1>Test2</h1>
+      </div>
+      <p
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // fontSize: '2em',
+          // color: '#010101',
+          border: '1px solid blue',
+          position: 'sticky',
+          zIndex: 5000,
+        }}
+      >
+        <FallingLines
+          // color="#4fa94d"
+          color="black"
+          width="100"
+          visible={true}
+          ariaLabel="falling-circles-loading"
+        />
+      </p>
+    </>
   );
 };
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import css from './Loader.module.css';
+import PropTypes from 'prop-types';
+
+import { FallingLines } from 'react-loader-spinner';
+
+export class Loader extends Component {
+  render() {
+    return (
+      <FallingLines
+        color="#4fa94d"
+        width="100"
+        visible={true}
+        ariaLabel="falling-circles-loading"
+      />
+    );
+  }
+}
+
+Loader.propTypes = {
+  isLoader: PropTypes.bool,
+};
